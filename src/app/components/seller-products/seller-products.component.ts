6775736ad3092f1adaf964b1b76f7d90e5f92e12
VeraZwr <vera.zwr@hotmail.com>
2020-04-20 16:22:18 -0400

    buttons added

---
+++ src/app/components/seller-products/seller-products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  image: number;
+  price: string;
+  stock: string;
+  description: string;
+  modify: string;
+  outofstock: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'fish', image: 3, price: '4.0', description: 'yumy', stock: '3', modify: 'm', outofstock: 'o'},
+  {position: 2, name: 'bird', image: 6, price: '7.0', description: 'eh', stock: '0', modify: 'm', outofstock: 'o'}
+];
+
+@Component({
+  selector: 'app-seller-products',
+  templateUrl: './seller-products.component.html',
+  styleUrls: ['./seller-products.component.scss']
+})
+export class SellerProductsComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'image', 'price', 'description', 'stock', 'modify', 'outofstock'];
+  dataSource = ELEMENT_DATA;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
