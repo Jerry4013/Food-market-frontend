5a8ef7d9d5a1d9ac87a65396d2f75b8a3bb0a1e1
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-04-23 13:33:20 -0400

    fix some bugs

--- src/app/components/user/signup/signup.component.ts
+++ src/app/components/user/signup/signup.component.ts
@@ -5,6 +5,10 @@ import {Router} from '@angular/router';
 import {UserModel} from '../../../models/user.model';
 import {ErrorModel} from '../../../models/error.model';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {Order} from '../../../models/order.model';
+import {ProductModel} from '../../../models/product.model';
+import {OrderService} from '../../../services/order.service';
+import {ProductService} from '../../../services/product.service';
 
 @Component({
   selector: 'app-signup',
@@ -18,6 +22,8 @@ export class SignupComponent implements OnInit {
   errMsg: string;
 
   constructor(private authService: AuthService,
+              private orderService: OrderService,
+              private productService: ProductService,
               private router: Router,
               private snackBar: MatSnackBar) { }
 
@@ -33,6 +39,27 @@ export class SignupComponent implements OnInit {
         this.authService.user = response.data as UserModel;
         this.user = this.authService.user;
         this.authService.userChanged.next(this.user);
+        this.orderService.listByBuyer(this.user.id).subscribe(response1 => {
+          if (response1.status === 'success' ) {
+            this.orderService.orderList = response1.data as Order[];
+          } else {
+            this.errMsg = (response1.data as ErrorModel).errMsg;
+          }
+        });
+        this.orderService.listByOwner(this.user.id).subscribe(response2 => {
+          if (response2.status === 'success' ) {
+            this.orderService.sellerOrderList = response2.data as Order[];
+          } else {
+            this.errMsg = (response2.data as ErrorModel).errMsg;
+          }
+        });
+        this.productService.listBySeller(this.authService.user.id).subscribe(response3 => {
+          if (response3.status === 'success' ) {
+            this.productService.sellerProducts = response3.data as ProductModel[];
+          } else {
+            this.errMsg = (response3.data as ErrorModel).errMsg;
+          }
+        });
         this.router.navigate(['/']).then();
         this.snackBar.open('New user created!', 'done', {
           duration: 2000,
